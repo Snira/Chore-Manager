1259e75eaa721001b707ff7f42ec88accdb85920
Snira <gbhavelaar@gmail.com>
2016-09-20 17:13:01 +0200

    Changed webtitle to Chore Manager, added some minor text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,14 @@
 
-
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <link href="{{ URL::asset('assets/css/app.css')}}" rel="stylesheet">
-    <title>Home</title>
-</head>
-
-
-
-<body>
-<div id="header" class="page-header">
-    <h1>Chore Manager</h1>
-</div>
-<div id="main" class="container-fluid">
-    <div class="row">
-        <p class="col-md-9">
-            Welcome
-
-        </p>
-    </div>
-</div>
-<div id="footer">
-
+@extends('layouts.app')
+@section('content')
+<div class="container-fluid">
+    Welcome to the home page of chore manager
 </div>
 
-</body>
-</html>
+
+
+
+
+
+
+@endsection
